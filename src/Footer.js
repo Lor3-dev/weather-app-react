7687aac410bd1553b06fe8c9c38f2e2afd99a3ac
Lor <lor.develop@gmail.com>
2021-04-09 15:58:41 +0200

    Changes for deploy

--- src/Footer.js
+++ src/Footer.js
@@ -2,10 +2,11 @@ import React from "react";
 export default function Footer() {
   return (
     <small className="footer">
+      This project was coded by Lorena Ibeas and is{" "}
       <a href="https://github.com/Lor3-dev/vanilla-weather-app" target="_blank">
-        Open-source
-      </a>
-      by Lorena Ibeas
+        Open-sourced on GitHub
+      </a>{" "}
+      and <a href="#">hosted on Netlify</a>
     </small>
   );
 }
